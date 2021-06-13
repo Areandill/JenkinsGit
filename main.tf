05b915240eded56d0768a5f247b793f37d72dd75
Areandill <areandill@gmail.com>
2021-06-13 08:00:04 +0000

    Update files for project

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
-provider "aws" {
-        region = "eu-central-1"                    
-}
+provider "aws" { region = "eu-central-1" }
 
 resource "aws_instance" "WebServer" {
     ami = "ami-05f7491af5eef733a"
     instance_type = "t2.micro"
     security_groups = ["Apache"]
+    
     provisioner "local-exec" {
     command = "echo ${aws_instance.app_server.public_ip} >> output.tf"
-  }
-
+    }
 tags = {
     Name = "WebServer"
     Owner = "Oleksii Martyniuk"
     Project = "Final Project"
- }
+  }
 }
