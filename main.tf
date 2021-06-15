ddd18ee5f906c279e39163f508e38600ce289b1c
Areandill <areandill@gmail.com>
2021-06-15 13:35:37 +0000

    Modifyed main and variables

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
-provider "aws" { 
-                 region = "eu-central-1" 
-                 access_key = "${var.access_key}"
-                 secret_key =  "${var.secret_key}" 
-               } 
-
+provider "aws" {
+        region = "${var.region}"      
+        access_key = "${var.access_key}"
+        secret_key = "${var.secret_key}"
+}
 resource "aws_instance" "WebServer" {
-    ami = "ami-05f7491af5eef733a"
+    ami = "${var.ami}"
     instance_type = "t2.micro"
+    key_name = "${var.key_name}"
     security_groups = ["Apache"]
-    
-    
+   
+
 tags = {
-    Name = "WebServer"
+    Name = "Apache web Server"
     Owner = "Oleksii Martyniuk"
     Project = "Final Project"
-  }
+ }
+  
 }
-output "pub_ips" {
+output "instance_ips" {
     value = aws_instance.WebServer.public_ip
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,23 @@
+variable "region" {
+    description = "Instance Region"
+    type = string
+    default = "eu-central-1"
+}
 variable "access_key" {
        description = "Access Key"    
 }
 variable "secret_key" {
        description = "Secret Key"
+}
+variable "ami" {
+    description = "ami image for instance"
+    default = "ami-05f7491af5eef733a"
+}
+variable "security_groups" {
+    description = "EC2 Security group"
+    default = ["Apache"]
+}
+variable "key_name" {
+    description = "Existing Instance private Key"
+    default = "EC2 Tutorial"
 }
